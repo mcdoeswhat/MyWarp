b1e1727105da4cffe1ba7b9c3b257795e9100395
mcdoeswhat <2929356483@qq.com>
2021-05-12 15:43:32 +0800

    Add files via upload

--- MyWarp/src/me/albert/mywarp/tabcompleter/WarpTabCompleter.java
+++ MyWarp/src/me/albert/mywarp/tabcompleter/WarpTabCompleter.java
@@ -3,29 +3,31 @@ package me.albert.mywarp.tabcompleter;
 import me.albert.mywarp.MyWarp;
 import me.albert.mywarp.Warp;
 import me.albert.mywarp.WarpUtil;
-import me.albert.mywarp.inventory.MyWarpInv;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
-import org.bukkit.util.StringUtil;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class WarpTabCompleter implements TabCompleter {
+
+    public static void main(String[] args) {
+        System.out.println("sk".startsWith("sk"));
+    }
+
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player){
-            if (args.length == 1 && MyWarp.getInstance().getConfig().getBoolean("enable-tab-complete")){
-                ArrayList<String> warpNames = new ArrayList<>();
+        if (sender instanceof Player) {
+            if (args.length == 1 && MyWarp.getInstance().getConfig().getBoolean("enable-tab-complete")) {
                 final List<String> completions = new ArrayList<>();
-                for (Warp warp : WarpUtil.warps){
-                    warpNames.add(warp.getName());
+                for (Warp warp : WarpUtil.warps) {
+                    if (warp.getName().toLowerCase().startsWith(args[0].toLowerCase())
+                            || warp.getName().toLowerCase().contains(args[0].toLowerCase())) {
+                        completions.add(warp.getName());
+                    }
                 }
-                StringUtil.copyPartialMatches(args[0], warpNames, completions);
-                Collections.sort(completions);
                 return completions;
             }
             return null;
